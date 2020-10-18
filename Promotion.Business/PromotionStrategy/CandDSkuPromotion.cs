1fa5ae87ca960f68c652a5fba4c65600c8701319
pramod-er123 <pramod_er123@yahoo.com>
2020-10-18 14:39:00 +0530

    Added impelementation of promotion

---
+++ Promotion.Business/PromotionStrategy/CandDSkuPromotion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Promotion.Core.Interface;
+using Promotion.Core.Domain;
+using System.Linq;
+
+namespace Promotion.Business.PromotionStrategy
+{
+    class CandDSkuPromotion : IPromotionStrategy
+    {
+        private double _offeredPrice { get; set; }
+        public CandDSkuPromotion(double offeredPrice)
+        {
+            _offeredPrice = offeredPrice;
+        }
+
+        public void Apply(List<OrderItem> items)
+        {
+            double priceAfterDiscount = 0;
+
+            var productsC = items.Where(x => x.product.SKU == "C").SingleOrDefault();
+            var productsD = items.Where(x => x.product.SKU == "D").SingleOrDefault();
+
+            if (productsC != null && productsD != null)
+            {
+                if (productsC.Quantity == productsD.Quantity)
+                {
+                    priceAfterDiscount = productsC.Quantity * _offeredPrice;
+                }
+                else if (productsC.Quantity > productsD.Quantity)
+                {
+                    int diff = productsC.Quantity - productsD.Quantity;
+                    priceAfterDiscount = (diff * _offeredPrice) + ((productsC.Quantity - diff) * productsC.ActualPrice);
+
+                }
+                productsC.DiscountedPrice = priceAfterDiscount;
+                productsD.DiscountedPrice = 0;
+                productsC.IsDiscountApplied = true;
+                productsD.IsDiscountApplied = true;
+            }
+
+        }
+    }
+}
---
+++ Promotion.Business/PromotionStrategy/ThreeOfASkuPromotion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Promotion.Core.Interface;
+using Promotion.Core.Domain;
+using System.Linq;
+
+namespace Promotion.Business.PromotionStrategy
+{
+    class ThreeOfASkuPromotion : IPromotionStrategy
+    {
+        private double _offeredPrice { get; set; }
+        public ThreeOfASkuPromotion(double offeredPrice)
+        {
+            _offeredPrice = offeredPrice;
+        }
+
+        public void Apply(List<OrderItem> items)
+        {
+            var productsA = items.Where(x => x.product.SKU == "A").SingleOrDefault();
+
+            if (productsA != null)
+            {
+                int quotient = (productsA.Quantity) / 3;
+                int remainder = (productsA.Quantity) % 3;
+
+                double price = (_offeredPrice * quotient) + (remainder * productsA.Price);
+                productsA.DiscountedPrice = price;
+                productsA.IsDiscountApplied = true;
+            }
+        }
+
+    }
+}
